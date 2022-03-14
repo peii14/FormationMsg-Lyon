b6f92bc591b1978f96909e1f4030de4ef55dfd26
Gayuh kautaman <gkoma444@gmail.com>
2022-03-14 09:51:53 +0100

    fix design reservez

--- pages/Reservez.tsx
+++ pages/Reservez.tsx
@@ -1,10 +1,13 @@
-const Reservez =() => {
-    return(
-        <div className="layout">
-            <main>
-                
-            </main>
-        </div>
-    )
+const Reservez = () => {
+  return (
+    <div className="layout">
+      <main>
+        <section className="pt-24">
+          <h2 className="text-center">Dates Formations Massage</h2>
+          <div></div>
+        </section>
+      </main>
+    </div>
+  )
 }
 export default Reservez
