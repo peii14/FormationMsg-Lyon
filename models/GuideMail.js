291ab76cfb77a1260264540e73da2ebcdf9bc771
Gayuh kautaman <gkoma444@gmail.com>
2022-06-09 20:15:32 +0200

    added email offer

--- models/GuideMail.js
+++ models/GuideMail.js
@@ -1,18 +1,13 @@
 
-const { timeStamp } = require('console')
 const mongoose = require('mongoose')
 const GuideSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'Please add an e-mail'],
-    unique: true,
-    trim: true,
+
   },
 
-},
-{
-  timestamps: true
-},
+}
 )
 module.exports =
   mongoose.models.Guide || mongoose.model('Guide', GuideSchema)
--- pages/api/offerGuide/index.tsx
+++ pages/api/offerGuide/index.tsx
@@ -1,38 +1,27 @@
 import dbConnect from '../../../utils/mongo'
 import Guide from '../../../models/GuideMail'
 
-dbConnect()
-
-export default async (
-  req: { body?: any; method?: any },
-  res: {
-    status: (arg0: number) => {
-      (): any
-      new (): any
-      json: { (arg0: { success: boolean; data?: any }): void; new (): any }
-    }
-  }
-) => {
+const handler = async (req: any, res: any) => {
   const { method } = req
 
-  switch (method) {
-    case 'GET':
-      try {
-        const offerGuide = await Guide.find({})
-        res.status(200).json({ success: true, data: offerGuide })
-      } catch (error) {
-        res.status(400).json({ success: false })
-      }
-      break
-    case 'POST':
-      try {
-        const offerGuide = await Guide.create(req.body)
-        res.status(201).json({ success: true, data: offerGuide })
-      } catch (error) {
-        res.status(400).json({ success: false })
-      }
-      break
-    default:
-      res.status(400).json({ success: false })
+  await dbConnect()
+
+  if (method === 'GET') {
+    try {
+      const guide = await Guide.find()
+      res.status(200).json(guide)
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  }
+  if (method === 'POST') {
+    try {
+      const guide = await Guide.create(req.body)
+      res.status(201).json(guide)
+    } catch (err) {
+      res.status(500).json(err)
+    }
   }
 }
+
+export default handler
