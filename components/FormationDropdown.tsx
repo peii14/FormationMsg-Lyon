c4648af298ceebac3626ceba76ef606143da6034
Adam Satria <satriaadam04@gmail.com>
2022-06-09 20:56:33 +0200

    Update FormationDropdown.tsx

--- components/FormationDropdown.tsx
+++ components/FormationDropdown.tsx
@@ -68,50 +68,64 @@ const FormationDropdown = () => {
       <Dropdown topic="Les Massages Habillés sans huiles" type="1">
         <div className="grid grid-cols-5 items-center gap-10 text-center ">
           <div className="grow-0">
-            <Modal title="Assis “Amma”" />
+            <Modal title="Assis “Amma”" content = "Durée: 2jours (petit Groupe de 2 à 6 Participants)"
+            content3 = "Réaliser une séance d'au moins 25 minutes de massage Amma assis professionnel, Massage Habillé sans Huile, Séance que vous adapterez soit plus longue ou plus courte, La durée moyenne d'un  massage assis est de 15mn."/>
           </div>
           <div className="grow-0">
-            <Modal title="Do In" />
+            <Modal title="Do In" content = "Durée: 2 jours"
+            content2 = "Nombre de participants: Formation de seulement 1 à 6 personnes pour un apprentissage de qualité."
+            content3 = "L'auto Massage « Do In » vous permettra chaque jour de faire bénéficier à vous même des bienfaits du massage. Vous découvrirez bien plus qu'une technique de massage mais un réel art de vivre ancestral."/>
           </div>
           <div className="grow-0">
-            <Modal title="Shiatsu" />
+            <Modal title="Shiatsu" content = "Durée:  3Jours  (1 à 6 personnes pour un apprentissage de qualité.)"
+            content3 = "Le Massage Shiatsu est une technique ancestrale japonaise qui s'effectue par pressions avec les doigts et se reçoit habillé au sol. Il permet de relâcher, d'évacuer des tensions corporelles, organiques, articulaires ou énergétiques."/>
           </div>
           <div className="grow-0">
-            <Modal title="Femme Enceinte" />
+            <Modal title="Femme Enceinte" content = "Durée: 3 jours"
+            content2 = "Nombre de participants: 2 à 6 personnes  pour un apprentissage de qualité"
+            content3 = "Acquérir les gestes techniques avec un protocole adapté à la position latérale, Connaître les différentes modalités d’installation pour un confort absolu de la femme enceinte et du praticien, Identifier les possibilités d’application et les limites d’intervention (modelage à but non thérapeutique et non médical). Connaître la physiologie et les étapes d’une grossesse. Savoir accueillir et répondre aux attentes d’une femme enceinte afin de créer un climat de confiance lors du massage."/>
           </div>
           <div className="grow-0">
-            <Modal title="Thaïlandais" />
+            <Modal title="Thaïlandais" content = "Durée: 1j, 2j, 3j, 4j ou 5j massage de 25mn à 2h"
+            content3 = "Le Massage Thaïlandais traditionnel est un massage relaxant qui tonifie et assouplit le corps.  Il est quotidiennement utilisé en Thaïlande pour ses vertus thérapeutiques. Techniques basées sur des pressions, des étirements et rotations. On le reçoit habillé au sol, allongé, sur le côté ou assis ce qui permet de le réaliser très facilement dans toute circonstance, cet enchaînement vous permet également d'apprendre à éffectuer des massages assis qui peuvent s'adapter sur une chaise. Cette formation vous permettra de réaliser"/>
           </div>
           <div className="grow-0">
-            <Modal title="Post Grossesse" />
+            <Modal title="Post Grossesse" content = "Durée:  3 Jours (Petit groupe de 2 à 6 personnes)"
+            content3 = "Les Bienfaits du Massage: Ce massage va permettre à la jeune maman de relacher les tensions physiques et psychologiques liées à la grossesse et l'accouchement, prendre un temps pour elle, se reconnecter avec son corps et ses ressentis."/>
           </div>
           <div className="grow-0">
-            <Modal title="Bébé Shantala" />
+            <Modal title="Bébé Shantala" content = "Durée:  2 Jours (Petit groupe de 2 à 6 personnes)"
+            content3 = "Les Bienfaits du massage: Ce massage pratiqué régulièrement aide les différents systèmes du bébé à se développer de façon optimum, il permet au bébé de prendre conscience de son corps et de ses limites, des liens de confiance très fort entre parent et enfant serons établis, il permet également au bébé de bien s'adapter au monde qui l'entoure et s’épanouir."/>
           </div>
           <div className="grow-0">
-            <Modal title="Massage de l’Enfant" />
+            <Modal title="Massage de l’Enfant" content = "Durée:  2 Jours  (1 à 6 personnes pour un apprentissage de qualité.)"
+            content3 = "Acquérir les différentes techniques permettant de pratiquer un massage bien-être de l'enfant sur les plans: sensoriel, psychomoteur, émotionnel et affectif, Découvrir les gestes spécifiques du massage de l'enfant, Accompagner les parents dans leur pratique et leur donner confiance, Accueillir un enfant et son parent pour une session de massage, Organiser un atelier Massage enfant en individuel ou en groupe."/>
           </div>
           <div className="grow-0">
-            <Modal title="Animation Atelier Enfant" />
+            <Modal title="Bébé &#38; Animation d'Atelier" content = "Durée: 2 jours"
+            content2 = "Nombre de participants: Formation de seulement 1 à 6 personnes pour un apprentissage de qualité."
+            content3 = "Acquérir les différents gestes permettant de pratiquer un massage bien être visant l'éveil du bébé sur les plans sensoriel, psychomoteur, émotionnel et affectif, Découvrir les gestes spécifiques aux petits maux des nourrissons, Accompagner les parents dans leur pratique et leur donner confiance, Savoir organiser un atelier en individuel ou en groupe, Apprendre à décoder les besoins des bébés lors de l’atelier, Découvrir les bienfaits et les contre-indications, Obtenir des outils pour vendre votre soin"/>
           </div>
           <div className="grow-0">
-            <Modal title="Bébé &#38; Animation d'Atelier" />
-          </div>
-          <div className="col-start-3 grow-0">
-            <Modal title="Assis “Amma” A distance" />
+            <Modal title="Assis “Amma” A distance" content = "Durée: 2jours (petit Groupe de 2 à 6 Participants)"
+            content2 = "Parties du corps Massés: Dos, Epaules Bras, Mains, Tête"
+            content3 = "Réaliser une séance d'au moins 25 minutes de massage Amma assis professionnel . Massage Habillé sans Huile, Séance que vous adapterez soit plus longue ou plus courte, La durée moyenne d'un  massage assis est de 15mn."/>
           </div>
         </div>
       </Dropdown>
       <Dropdown topic="Massages &#38; Réflexologie" type="1">
         <div className="grid grid-cols-5 items-center gap-10">
           <div className="grow-0">
-            <Modal title="Massage Etoile" />
+            <Modal title="Massage Etoile" content = "Durée: 2 Jours (de 1 à 6 personnes)"
+            content2 = "Objectif: réaliser un massage d'au moins 40min"
+            content3 = "Cette technique de massage appelé aussi le massage Etoile vous permettra de découvrir les mécanismes de la réflexologie et de proposer des  massages de détente d'au moins 40 minutes en utilisant des techniques qui apportent une détente profonde Vous apprendrez également comment soulager les maux les plus courants."/>
           </div>
           <div className="grow-0">
-            <Modal title="Ventre Chi Nei Tsang" />
+            <Modal title="Ventre Chi Nei Tsang" content = "Durée: 3 Jours  (Petit groupe de 2 à 6 personnes)"
+            content3 = "Les bienfaits du massage: Ce massage va permettre au corps de relâcher les tensions physiques et psychologiques localisées dans le ventre. Il permet d'améliorer certains problèmes digestifs mineurs (Constipation, spasmes, inflammations...) et de réduire les douleurs lombaires et dorsales."/>
           </div>
           <div className="grow-0">
-            <Modal title="Visage Indien Marmas" />
+            <Modal title="Visage Indien Marmas" content3 = "Marma signifie «caché » - «secret ». Les points Marmas sont des points du corps qui sont transmis en Inde de maitre à disciple. Ce sont des points de jonction entre les os, les muscles et les ligaments.  Ces points sont des points de santé qui lorsqu'ils sont stimulés ont la capacité de renforcer le système immunitaire, faciliter la circulation de l'énergie et ainsi éviter des maladies."/>
           </div>
           <div className="grow-0">
             <Modal title="Kobido" />
